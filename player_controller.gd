d7fd54c2e9e0a97730b75e37c2418fcb0d6c234f
jakebrandon660 <jakebrandon660@gmail.com>
2026-01-19 15:04:40 -0800

    Export Variables + Basic WASD movement
    Can add and change anything needed, wasn't entirely sure how the 8-d movement and stuff were meant to feel like, so I kept it pretty basic.

--- player_controller.gd
+++ player_controller.gd
@@ -1,7 +1,15 @@
 extends CharacterBody3D
 
+#Export variables (add more if needed)
+#the max amount of speed the player can reach
+@export var max_speed: float = 5.0
+#the speed at which the player speeds up
+@export var acceleration: float = 20.0
+#friction is the speed at which the player decelerates
+@export var friction: float = 20.0
+#Gravity is currently not doing anything right now
+@export var gravity: float = 0.0
 
-@export var SPEED: float =  5.0
 @export var JUMP_VELOCITY: float = 4.5
 
 
@@ -16,13 +24,17 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		#
+		velocity.x = move_toward(velocity.x, direction.x * max_speed, acceleration * delta)
+		velocity.z = move_toward(velocity.z, direction.z * max_speed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+		velocity.z = move_toward(velocity.z, 0, friction * delta)
+	
+	
 	move_and_slide()
