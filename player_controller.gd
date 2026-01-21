fbe539d3ba295f1dcc8777cc91a62439d636e86c
Eli Chen <jiananchen0@gmail.com>
2026-01-20 17:04:40 -0800

    toggle left added
    "space" input map
    allow left toggle

--- player_controller.gd
+++ player_controller.gd
@@ -12,6 +12,7 @@ extends CharacterBody3D
 
 @export var JUMP_VELOCITY: float = 4.5
 
+@export var allow_left: bool = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,14 +20,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	if not allow_left:
+		input_dir.x = max(input_dir.x, 0)
+		
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
 		#
