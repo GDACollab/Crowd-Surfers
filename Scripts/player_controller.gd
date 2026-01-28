e6b56e43512082c8968bd1e2ea3048e0d79878b1
Joseph Dadlez <jsdadlez@outlook.com>
2026-01-27 18:06:23 -0800

    Added current_max_speed
    this if for people who need to mess with max_speed with abilities

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -3,8 +3,12 @@ extends CharacterBody3D
 
 #Export variables (add more if needed)
 #the max amount of speed the player can reach
-@export var max_speed: float = 5.0
+@export var base_max_speed: float = 5.0
 #the speed at which the player speeds up
+
+#speed that can be edited by the abilitys.
+@export var current_max_speed: float = 5.0
+
 @export var acceleration: float = 20.0
 #friction is the speed at which the player decelerates
 @export var friction: float = 20.0
@@ -29,28 +33,23 @@ var coyoted : bool = false
 
 func coyote_toggle() -> void:
 	if (!coyoted):
-		print("turning on")
 		in_coyote = true
 		await get_tree().create_timer(coyote_time).timeout 
-		print("turning off")
 		in_coyote = false
 
 func _physics_process(delta: float) -> void:
 	# Set player sprite offset
-	#print("we bricked it")
 	var offset = global_position.y
 	player_sprite.position.z = -offset
-	print("Player offset", -offset)
 	
 	# Add the gravity.
 	if not is_on_floor() :
 		velocity += gravity * Vector3.DOWN * delta
-		
 		# calls a method that toggle
 		# need to make this call once
-		
 		coyote_toggle()
 		coyoted = true	
+		#result = move_toward(current, target, delta)
 	else :
 		jumped = false
 		coyoted = false
@@ -58,12 +57,10 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("move_jump") and (is_on_floor() or in_coyote) and !jumped :
 		velocity.y = JUMP_VELOCITY
-		
 		jumped = true
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	print("working")
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if not allow_left:
@@ -73,12 +70,11 @@ func _physics_process(delta: float) -> void:
 	
 	if direction:
 		#
-		velocity.x = move_toward(velocity.x, direction.x * max_speed, acceleration * delta)
-		velocity.z = move_toward(velocity.z, direction.z * max_speed, acceleration * delta)
+		velocity.x = move_toward(velocity.x, direction.x * base_max_speed, acceleration * delta)
+		velocity.z = move_toward(velocity.z, direction.z * base_max_speed, acceleration * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 		velocity.z = move_toward(velocity.z, 0, friction * delta)
-	
-	
+
 	
 	move_and_slide()
