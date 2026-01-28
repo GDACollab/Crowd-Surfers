b9433ddc2ccf9e68c709d47b52524fd8ae3d4a42
ChristopherTGreen <ctgreen00@gmail.com>
2026-01-28 15:09:08 -0800

    Implemented Timed & Cancellable Glide
    - Implemented a timed glide ability, which decreases player gravity to 0 and Y velocity to 0, but gravity is customizable until reverting back.
    - Ability to glide only possible while in air, and can be cancelled in the air.
    - If cancelled in air, can not be called until the timer duration is up.
    - Settings for decreased gravity & duration is high, for demonstration purposes.
    - Currently, the ability recharge/reuse is tied to duration, not on if you touch the ground.

--- Scripts/GlideAbility.gd
+++ Scripts/GlideAbility.gd
@@ -1,9 +1,10 @@
 class_name grav_mult extends Ability
 
-@export var gravityMultiplier: float = 4.0
+@export var gravityMultiplier: float = 0.0
 var originalVal: float
 var myPlayer: CharacterBody3D
 var activated: bool = false
+var available: bool = true
 
 # TEMPLATE FOR PASSIVE ABILITIES! (ie abilities that are activated for a small portion of time)
 # Reference and modify as you'd like :)d 
@@ -14,19 +15,26 @@ var activated: bool = false
 # Make sure to save the base value and modify the current value, then revert it later
 
 func Use(player: CharacterBody3D):
-	if !activated && !player.is_on_floor():
+	if !activated && !player.is_on_floor() && available:
 		originalVal = player.gravity
 		
-		player.gravity /= gravityMultiplier
+		player.gravity *= gravityMultiplier
+		player.velocity.y = 0
 		
 		myPlayer = player
 		$Timer.start(duration)
 		activated = true
+	# Checks if the player calls mid-air to cancel flight, won't allow them 
+	# to call it until timer duration is finished.
+	elif activated:
+		available = false
+		Exit(myPlayer)
 
 func Exit(player: CharacterBody3D):
 	player.gravity = originalVal
 
 func _on_timer_timeout() -> void:
 	Exit(myPlayer)
+	available = true
 	activated = false;
 	pass # Replace with function body.
