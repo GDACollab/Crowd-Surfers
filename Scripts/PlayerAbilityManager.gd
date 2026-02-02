e5a5bc4f0c2de132162d3cf3f1ecbd3bdef1134d
FFreeze <quincymon@icloud.com>
2026-02-02 13:47:25 -0800

    Support for restoring momentum when dashing upon landing
    I had to add infrastructure for the ability manager to know which abilities are active and to unload them as necessary. If other teams like the system they can use it but if not just integrate my logic into whatever you have working

--- Scripts/PlayerAbilityManager.gd
+++ Scripts/PlayerAbilityManager.gd
@@ -4,6 +4,8 @@ extends Node
 
 @export var myPlayer: CharacterBody3D
 
+var activeAbilities: Array[int]
+
 func _process(_delta: float) -> void:
 	var inputs = checkAbilities()
 	for i in inputs:
@@ -12,8 +14,24 @@ func _process(_delta: float) -> void:
 			# This node needs a player_controller as a parent!
 			# Nothing happens if it's not
 			abilities[i].Use(get_parent())
+			activeAbilities.append(i)
 		else:
 			print("You still need to set the player in the ability manager!")
+			
+	# Prepare list of abilities to deactivate
+	var abilitiesToDeactivate: Array[int]
+	# Check each active ability
+	for i in activeAbilities:
+		# Deactivate stomp is player is on the floor
+		if abilities[i] == $StompAbility and myPlayer.is_on_floor():
+			$StompAbility.Exit(myPlayer)
+			abilitiesToDeactivate.append(i)
+			
+	# Amend list of active abilities
+	for i in abilitiesToDeactivate:
+		activeAbilities.erase(i)
+	
+	
 
 # This checks user inputs to determine what abilities to call this frame
 func checkAbilities() -> Array[int]:
