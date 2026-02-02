a87aa867d08362057f883deb2f0ac67dae7f2019
JustinVFong <justinvf2018@gmail.com>
2026-02-02 13:54:27 -0800

    dash update 3
    - added @export toggle for enabling mid-air dash
    
    - dashing in the air maintains applies stored momentum
    - dashing on ground doesn't apply stored momentum
    
    - speed burst based on dash_speed and stored momentum

--- Scripts/DashAbility.gd
+++ Scripts/DashAbility.gd
@@ -6,8 +6,10 @@ extends Ability
 ## The temporary speed boost that the player gains on ability use. This boost is relative to current player speed.
 @export var dash_speed : float = 1.2
 
-## The cooldown (in seconds) where this ability becomes unusable until cooldown is complete.
+## The cooldown (in seconds) where this ability becomes unusable until cooldown is complete. Different than the duration.
 @export var dash_cooldown : float = 2.0
+
+@export var can_dash_midair : bool = true
 
 var original_val : float
 var my_player: CharacterBody3D
@@ -23,26 +25,30 @@ func Use(player: CharacterBody3D):
 
 		var direction := (player.transform.basis * Vector3(newest_dir_input.x, 0, newest_dir_input.y))
 		
-		# If you dash into a glide/airborne, then momentum should not be removed.
-		# Dashes should allow instant redirection, all other momentum is routed to point at directional input.
 		# Change velocity from current direction and have it move towards the directional input.
-		if direction:
-			player.velocity.x = move_toward(0.0, direction.x * stored_momentum, dash_length)
-			player.velocity.z = move_toward(0.0, direction.z * stored_momentum, dash_length)
-		else:
-			print("Input recorded as ZERO")
-			
+		if !direction:
+			print("New Input recorded as ZERO.")
+		
+		# If you dash into a glide/airborne, then momentum should not be removed.
 		if not player.is_on_floor():
-			print("Dash in AIR")
+			
+			if can_dash_midair:
+				#print("Dash in AIR")
+				player.velocity.x = move_toward(0.0, direction.x * stored_momentum, dash_length)
+				player.velocity.z = move_toward(0.0, direction.z * stored_momentum, dash_length)
+			else:
+				print("Midair dash disabled.")
 		else:
-			print("Dash on GROUND")
-		
-		
+			#print("Dash on GROUND")
+			player.velocity.x = move_toward(0.0, direction.x, dash_length)
+			player.velocity.z = move_toward(0.0, direction.z, dash_length)
+			
 		# Burst of speed is relative to your current speed
 		original_val = player.base_max_speed
 		player.current_max_speed = player.base_max_speed * dash_speed + stored_momentum
 		
-		# Start timers
+		# Start timers, implemented with seperate timers in case cooldown and duration of the speed boost
+		# are intended to last at different times, (i.e. CD @ 3 seconds, Duration @ 2.5 seconds)
 		$Duration.start(duration)
 		$Cooldown.start(dash_cooldown)
 		activated = true
