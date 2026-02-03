6d32cbfcaa1712cbd8c1586eb7369945a80dbd81
Tate Maguire <tatemaguir@gmail.com>
2026-02-02 20:09:49 -0800

    modified fmod events, moved and changed fmod-demo, and added stomp sound to the stomp ability

--- Scenes/Ability Manager/stomp_ability.gd
+++ Scenes/Ability Manager/stomp_ability.gd
@@ -1,13 +1,16 @@
 class_name stomp_ability extends Ability
 
 @export var stomp_speed: float
-
 # Player's momentum in the xz-plane just before activating stomp
 var player_original_momentum: Vector2 = Vector2(0, 0)
+
+# References
+@onready var stomp_sound: FmodEventEmitter3D = $StompSound
 
 func Use(player: CharacterBody3D) -> void:
 	player_original_momentum = Vector2(player.velocity.x, player.velocity.z)
 	player.velocity = Vector3(0,stomp_speed,0)
+	stomp_sound.play()
 	
 func Exit(_player: CharacterBody3D) -> void:
 	pass
