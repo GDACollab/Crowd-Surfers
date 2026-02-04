8f5988e26afa70a74bc45b657b7a4cbb7c942301
NinjaFromConsole <rothtyler24@gmail.com>
2026-02-04 11:42:32 -0800

    Speed up credits keybind
    - Pressing space in the credits now speeds up the scrolling speed
    . - There is a smooth transition between fast and slow scrolling

--- Scripts/credits.gd
+++ Scripts/credits.gd
@@ -1,19 +1,25 @@
 extends Control
 @onready var credits_text_container: VBoxContainer = $CreditsTextContainer
 
-@export var scroll_speed: float = 5.0
+@export var slow_scroll_speed: float = 50.0
+@export var fast_scroll_speed: float = 250.0
+@export var scroll_speed_change_rate_per_second: float = 250.0
 @export var separator_size: float = 40.0
 @export var credits_starting_y: float = 700
 
 @export var credits_text_file_string: String = ""
 
+#Used for a smooth transition between slow and fast scroll speeds
+var current_scroll_speed: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	current_scroll_speed = slow_scroll_speed
 	self.position.y = credits_starting_y
 	
 	var credits_file: FileAccess = FileAccess.open(credits_text_file_string, FileAccess.READ)
 	
-	# Skip guide lines
+	# Skip guide lines (amazing code) (wow!)
 	credits_file.get_line()
 	credits_file.get_line()
 	credits_file.get_line()
@@ -43,4 +49,9 @@ func _ready() -> void:
 		credits_text_container.add_child(label)
 		
 func _process(delta: float):
-	self.position.y += scroll_speed * -1 * delta
+	if (Input.is_action_pressed("speed_up_credits") and current_scroll_speed < fast_scroll_speed):
+		current_scroll_speed += scroll_speed_change_rate_per_second * delta
+	elif (!Input.is_action_pressed("speed_up_credits") and current_scroll_speed > slow_scroll_speed):
+		current_scroll_speed -= scroll_speed_change_rate_per_second * delta
+	self.position.y += current_scroll_speed * -1 * delta
+	#print(current_scroll_speed)
