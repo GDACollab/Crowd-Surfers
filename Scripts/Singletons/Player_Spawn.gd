03aa2cd3ec1e2bf394279a0d70a58721d6ebbacf
cncz42 <zwickson42@gmail.com>
2026-02-12 21:13:57 -0800

    fix spawn point

--- Scripts/Singletons/Player_Spawn.gd
+++ Scripts/Singletons/Player_Spawn.gd
@@ -1,3 +1,4 @@
 extends Node
 
-var spawnpoint: Vector3 
+#quick fix of the spawnpoint singleton by spawning a bit above 0, presumably this bit's still WIP
+var spawnpoint: Vector3 = Vector3(0,0.1,0)
