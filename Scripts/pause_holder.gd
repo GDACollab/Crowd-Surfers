31ca6ffb39f62da1b7a582f3238bb595d7d2c349
NinjaFromConsole <rothtyler24@gmail.com>
2026-02-17 19:54:31 -0800

    Pause Screen Improvements
    - Pressing escape in the pause menu now closes the menu
    - Used get_tree().paused instead of changing timescale

--- Scripts/pause_holder.gd
+++ Scripts/pause_holder.gd
@@ -3,7 +3,12 @@ extends Control
 @export var pause_screen_scene: PackedScene
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("open_pause_menu") and get_child_count() == 0:
-		var pause_screen: Node = pause_screen_scene.instantiate()
-		add_child(pause_screen)
-		Engine.time_scale = 0.0
+	if Input.is_action_just_pressed("open_pause_menu"):
+		if get_child_count() == 0:
+			var pause_screen: Node = pause_screen_scene.instantiate()
+			add_child(pause_screen)
+			get_tree().paused = true
+		else:
+			get_tree().paused = false
+			get_child(0).queue_free()
+	
--- Scripts/pause_screen.gd
+++ Scripts/pause_screen.gd
@@ -1,6 +1,5 @@
 extends Control
 
 func _on_resume_button_pressed() -> void:
-	print("hi")
-	Engine.time_scale = 1.0
+	get_tree().paused = false
 	queue_free()
