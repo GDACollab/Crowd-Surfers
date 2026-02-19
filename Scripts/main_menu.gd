20b3b9a24bf5d32329ee5ab5b9c375cbbfe4156d
Toni <tonimunshoenen@gmail.com>
2026-02-19 00:50:01 -0800

    Transition Scene Orders Button

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_exit_button_pressed() -> void:
 	SceneFadeTransition.transition_to_scene(load("res://Scenes/UI Menus/TitleScreen.tscn"))
+
+func _on_orders_button_pressed() -> void:
+	SceneFadeTransition.transition_to_scene(load("res://Scenes/UI Menus/MainMenu/OrdersScene.tscn"))
