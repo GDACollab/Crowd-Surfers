889f2f3ebc34cc7f75b53651cdd73f30900294fc
NinjaFromConsole <rothtyler24@gmail.com>
2026-02-20 19:52:15 -0800

    Controls Rotate + Restart Level
    - Clicking on the controls menu now rotates the phone to landscape mode
    . - Rotated the text/back button to accommodate for this
    - Added a restart level button which... restarts your level

--- Scripts/pause_screen.gd
+++ Scripts/pause_screen.gd
@@ -1,8 +1,13 @@
 extends Control
 
-@onready var controls_container: VBoxContainer = $ControlsContainer
-@onready var button_container: VBoxContainer = $ButtonContainer
+@onready var phone_animation_player : AnimationPlayer = $PhoneBackground/AnimationPlayer
 
+@onready var controls_container: VBoxContainer = $PhoneBackground/ControlsContainer
+@onready var button_container: VBoxContainer = $PhoneBackground/ButtonContainer
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+	
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 	queue_free()
@@ -10,7 +15,14 @@ func _on_resume_button_pressed() -> void:
 func _on_controls_button_pressed() -> void:
 	controls_container.visible = true
 	button_container.visible = false
+	phone_animation_player.play("rotate_landscape")
 
 func _on_back_button_pressed() -> void:
 	controls_container.visible = false
 	button_container.visible = true
+	phone_animation_player.play("rotate_portrait")
+
+func _on_restart_level_button_pressed() -> void:
+	SceneFadeTransition.transition_to_scene(load(get_tree().current_scene.scene_file_path))
+	get_tree().paused = false
+	queue_free()
