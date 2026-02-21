cf2f76834432aefeeb7410bff3410fd70aca1d49
NinjaFromConsole <rothtyler24@gmail.com>
2026-02-21 00:37:14 -0800

    Minor Improvements
    - Minor format improvements to pause menu
    - Fixed being able to click on the controls and back button while the rotation animation was ongoing

--- Scripts/pause_screen.gd
+++ Scripts/pause_screen.gd
@@ -5,6 +5,8 @@ extends Control
 @onready var controls_container: VBoxContainer = $PhoneBackground/ControlsContainer
 @onready var button_container: VBoxContainer = $PhoneBackground/ButtonContainer
 
+var rotating: bool = false
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 	
@@ -13,11 +15,19 @@ func _on_resume_button_pressed() -> void:
 	queue_free()
 
 func _on_controls_button_pressed() -> void:
+	if (rotating):
+		return
+	
+	rotating = true
 	controls_container.visible = true
 	button_container.visible = false
 	phone_animation_player.play("rotate_landscape")
 
 func _on_back_button_pressed() -> void:
+	if (rotating):
+		return
+		
+	rotating = true
 	controls_container.visible = false
 	button_container.visible = true
 	phone_animation_player.play("rotate_portrait")
@@ -26,3 +36,7 @@ func _on_restart_level_button_pressed() -> void:
 	SceneFadeTransition.transition_to_scene(load(get_tree().current_scene.scene_file_path))
 	get_tree().paused = false
 	queue_free()
+
+# Called by animation player
+func stop_rotating() -> void:
+	rotating = false
