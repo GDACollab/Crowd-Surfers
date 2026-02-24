c37e0a62dbade654e31c83a98ec3ac7af30f8db3
Tate Maguire <tatemaguir@gmail.com>
2026-02-23 23:41:09 -0800

    fixed DebugMenu to work alongside pause menu

--- Scripts/DebugButton.gd
+++ Scripts/DebugButton.gd
@@ -5,3 +5,8 @@ extends Control
 # When the debug menu is pressed, the menu pops up
 func _on_debug_menu_button_pressed() -> void:
 	scroll_con.visible = not scroll_con.visible
+	
+func _process(_delta: float) -> void:
+	var parent = get_parent()
+	if (get_index() < parent.get_child_count()):
+		move_to_front()
