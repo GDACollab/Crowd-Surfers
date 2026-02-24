4257aa0d4ebd0939452add3178a6b7bc94850fb0
Tate Maguire <tatemaguir@gmail.com>
2026-02-23 19:00:33 -0800

    cleaned debug button script

--- Scripts/DebugButton.gd
+++ Scripts/DebugButton.gd
@@ -1,12 +1,8 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var scroll_container = $ScrollContainer
 
 # When the debug menu is pressed, the menu pops up
 func _on_debug_menu_button_pressed() -> void:
-	if (get_node("ScrollContainer").is_visible()): get_node("ScrollContainer").visible = false
-	elif (!get_node("ScrollContainer").is_visible()): get_node("ScrollContainer").visible = true
-	pass # Replace with function body.
+	if (scroll_container.is_visible()): scroll_container.visible = false
+	elif (scroll_container.is_visible()): scroll_container.visible = true
