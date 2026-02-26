b26fbb370b422976ae8670f04a6af07f17bdec60
NinjaFromConsole <rothtyler24@gmail.com>
2026-02-25 17:20:10 -0800

    Scale Fix + Instant Orders
    - Clicking on orders now instantly changes to the orders screen instead of fading
    - Fixed main menu being too small due to 1920x1080 change

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,7 +1,11 @@
 extends Control
 
+@onready var main_container: VBoxContainer = $MainContainer
+@onready var orders_container: ScrollContainer = $OrdersContainer
+
 func _on_exit_button_pressed() -> void:
 	SceneFadeTransition.transition_to_scene(load("res://Scenes/UI Menus/TitleScreen.tscn"))
 
 func _on_orders_button_pressed() -> void:
-	SceneFadeTransition.transition_to_scene(load("res://Scenes/UI Menus/MainMenu/OrdersScene.tscn"))
+	main_container.visible = false
+	orders_container.visible = true
