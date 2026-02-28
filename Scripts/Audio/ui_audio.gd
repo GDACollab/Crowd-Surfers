38e748d31b05f3295ab8c856e17779708290c004
Tate Maguire <tatemaguir@gmail.com>
2026-02-28 13:41:57 -0800

    level_audio and ui_audio scenes

---
+++ Scripts/Audio/ui_audio.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func play_confirm_sound() -> void:
+	$ConfirmSound.play()
+
+func play_back_sound() -> void:
+	$BackSound.play()
+
+func play_hover_sound() -> void:
+	$HoverSound.play()
