ededa9feeabe172f6bdf47d16700296a0e954fd3
Tate Maguire <tatemaguir@gmail.com>
2026-02-28 14:22:21 -0800

    redid ui audio; now uses references to buttons to connect signals, rather than relying on manual signal connections
    this basically undid my past few commits of manually connecting signals lol

--- Scripts/Audio/ui_audio.gd
+++ Scripts/Audio/ui_audio.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+## all buttons that make 'confirm' sound when clicked
+@export var confirm_buttons: Array[Button]
+## all buttons that make 'back' sound when clicked
+@export var back_buttons: Array[Button]
+
+func _ready() -> void:
+	# connect audio playback to all confirm buttons
+	for button in confirm_buttons:
+		button.mouse_entered.connect(play_hover_sound)
+		button.pressed.connect(play_confirm_sound)
+	
+	# connect audio playback to all back buttons
+	for button in back_buttons:
+		button.mouse_entered.connect(play_hover_sound)
+		button.pressed.connect(play_back_sound)
+
 func play_confirm_sound() -> void:
 	$ConfirmSound.play()
 
