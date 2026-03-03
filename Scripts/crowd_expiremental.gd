1fd9211bdec4ba3c0a1ec48ee802ca471cfe9bb7
DapperCoat <mrdappers12@gmail.com>
2026-03-02 21:05:56 -0800

    Testing

--- Scripts/crowd_expiremental.gd
+++ Scripts/crowd_expiremental.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
-
-
+# if in dash state ---> reset cooldown & give speed boost
+# if walked into ---> slow player speed by x percent
 func _on_area_3d_area_entered(body: Node3D) -> void:
-	if (body.name == "Player"):
-		
-		pass
+	if (body.name == "Player" && (body.current_state == "Ground Dash" || body.current_state == "Air Dash")):
+		print("Crowd Dash")
+	elif (body.name == "Player"):
+		print("Crowd Walk")
