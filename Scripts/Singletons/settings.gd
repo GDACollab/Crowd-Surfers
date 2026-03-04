121edc6f1d7ebb7b3bcee0bb7e5ae4c7aad50dc5
NinjaFromConsole <rothtyler24@gmail.com>
2026-03-03 21:07:08 -0800

    Fix volume not being consistent across scenes
    - idk why tat happens man

--- Scripts/Singletons/settings.gd
+++ Scripts/Singletons/settings.gd
@@ -4,19 +4,20 @@ extends Node
 var master_volume: float = 1.0;
 var music_volume: float = 1.0;
 var sfx_volume: float = 1.0;
-
-# Get bus paths from FMOD
-@onready var master_bus: FmodBus = FmodServer.get_bus("bus:/")
-@onready var music_bus: FmodBus = FmodServer.get_bus("bus:/MUS")
-@onready var sfx_bus: FmodBus = FmodServer.get_bus("bus:/SFX")
-
-# Other bus paths if we want more options:
-# "bus:/SFX/ENV"
-# "bus:/SFX/Player"
-# "bus:/SFX/UI"
-
+	
 func update_fmod_volumes():
+	#For some reason the busses cant be @onready, it didnt work when switching between (or reloading) scenes
+	# Other bus paths if we want more options:
+	# "bus:/SFX/ENV"
+	# "bus:/SFX/Player"
+	# "bus:/SFX/UI"
+	var master_bus: FmodBus = FmodServer.get_bus("bus:/")
+	var music_bus: FmodBus = FmodServer.get_bus("bus:/MUS")
+	var sfx_bus: FmodBus = FmodServer.get_bus("bus:/SFX")
+
 	# update bus volume property
 	master_bus.set("volume", master_volume)
 	music_bus.set("volume", music_volume)
 	sfx_bus.set("volume", sfx_volume)
+	
+	
