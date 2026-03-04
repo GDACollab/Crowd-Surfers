97088f5b6d7185ecf2e71e448d94820019df9c33
Tate Maguire <tatemaguir@gmail.com>
2026-03-03 19:08:17 -0800

    attached volume settings to fmod bus volumes

--- Scripts/Singletons/settings.gd
+++ Scripts/Singletons/settings.gd
@@ -5,9 +5,11 @@ var master_volume: float = 1.0;
 var music_volume: float = 1.0;
 var sfx_volume: float = 1.0;
 
+@onready var master_bus: FmodBus = FmodServer.get_bus("bus:/")
+@onready var music_bus: FmodBus = FmodServer.get_bus("bus:/MUS")
+@onready var sfx_bus: FmodBus = FmodServer.get_bus("bus:/SFX")
+
 func update_fmod_volumes():
-	var music_volume_final: float = master_volume * music_volume
-	var sfx_volume_final: float = master_volume * sfx_volume
-	
-	# fmodsetbusthingy(music_bus, music_volume_final)
-	# fmodsetbusthingy(sfx_bus, sfx_volume_final)
+	master_bus.set("volume", master_volume)
+	music_bus.set("volume", music_volume)
+	sfx_bus.set("volume", sfx_volume)
