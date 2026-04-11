580ee4469fe54f601289b0124b02707c34fc5dfa
Logan MacAskill <logan@macaskill.com>
2026-04-10 19:00:12 -0700

    more work towards working shader

--- Scripts/camera_3d.gd
+++ Scripts/camera_3d.gd
@@ -5,14 +5,12 @@ extends Camera3D
 @export var follow_speed: float = 0.5
 
 @onready var player: CharacterBody3D = get_node(player_path)
-@onready var ray: RayCast3D = get_node(ray_path)
 
 @export var offset: Vector3 = Vector3(0, 10000, 10000)
 
 @export var occlusionShader: ShaderMaterial
 
 var lead_smoothed: Vector3 = Vector3.ZERO
-
 
 func _process(delta: float) -> void:
 	var v: Vector3 = player.velocity
@@ -23,14 +21,8 @@ func _process(delta: float) -> void:
 	)
 	global_position = player.global_position + offset + lead_smoothed
 	
-	var player_view_pos = self.global_transform.affine_inverse() * player.global_position
+	var player_view_pos = -lead_smoothed
+	
+	#print(occlusionShader.get_property_list())
+	
 	occlusionShader.set_shader_parameter("player_view_pos", player_view_pos)
-	
-	
-	#
-	#ray.force_raycast_update()
-	#var collider = ray.get_collider()
-	#if collider:
-		#for child in collider.get_children():
-			#if child is Sprite3D:
-				#child.hide()
