957cc29b50b54be7a5ecb394198ae736b8f52689
DapperCoat <mrdappers12@gmail.com>
2026-04-15 16:15:59 -0700

    Merge branch 'main' into D-LevelConcepting-Act2

--- Scripts/obstacle_movement.gd
+++ Scripts/obstacle_movement.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
+## README NERDS
+# Hi, Ashton here! If your car for some reason isn't moving the player with it,
+# Make sure that you've changed the 'staticbody3D' to an 'animatablebody3D'
+# in the prefab!
+# Once you've done that, the rest of everything should work fine!
+
 ######################################
 # DYNAMIC MOVEMENT STUFFS WITH AARON #
 ######################################
@@ -12,9 +18,17 @@ extends Node3D
 @export var startFromSelf: bool = true # Doesnt work right now
 @export var isClosed: bool = true
 @export var isRotate: bool = false
+@export var isVehicle : bool = true
+
+var isMoving: bool = false
+var travel_speed: float = 0.0
 
 var follow_node: PathFollow3D
-var travel_speed: float = 0.0
+var curve: Curve3D
+
+#This serves to scale the velocity being applied to the player
+@export var jumpVelocityRatio: float = 0.8 
+var myVelocity: Vector3 = Vector3.ZERO
 
 #@export var active: bool = false:
 #	set(value): 
@@ -34,7 +48,10 @@ func _ready() -> void:
 #	if Engine.is_editor_hint():
 #		active = false
 	await get_tree().process_frame
+	if isVehicle:
+		self.add_to_group("Vehicles")
 	create_obstacle_path()
+	
 
 func create_obstacle_path() -> void:
 	
@@ -83,16 +100,17 @@ func create_obstacle_path() -> void:
 		obstacleCurve.set_point_out(i, handle_out)
 	
 	obstaclePath.curve = obstacleCurve
+	curve = obstacleCurve
 	
 	var obstaclePathFollow: PathFollow3D = PathFollow3D.new()
 	obstaclePathFollow.loop = true #So that the tween doesnt encounter any issues
 	obstaclePathFollow.rotation_mode = PathFollow3D.ROTATION_XYZ if isRotate else PathFollow3D.ROTATION_NONE
 	obstaclePath.add_child.call_deferred(obstaclePathFollow)
 	
-	self_node.reparent.call_deferred(obstaclePathFollow)
+	#self_node.reparent.call_deferred(obstaclePathFollow)
 	
 	follow_node = obstaclePathFollow
-	var obstaclePathLength = obstacleCurve.get_baked_length()
+	#var obstaclePathLength = obstacleCurve.get_baked_length()
 	travel_speed = 40.0 * tweenTravelSpeedRatio
 	
 	#if not startFromSelf:
@@ -100,25 +118,57 @@ func create_obstacle_path() -> void:
 		#var closest_offset = obstacleCurve.get_closest_offset(first_child_pos)
 		#obstaclePathFollow.progress = closest_offset
 	
-	await get_tree().process_frame
-	await get_tree().process_frame
-	
-	tween_movement(obstaclePathFollow, obstaclePathLength / travel_speed)
+	isMoving = true
+	#tween_movement(obstaclePathFollow, obstaclePathLength / travel_speed)
 
-func tween_movement(target_follow: PathFollow3D, pLen: float):
+#func tween_movement(target_follow: PathFollow3D, pLen: float):
+	#
+	#var tween = create_tween()
+	#
+	#tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	#tween.set_trans(Tween.TRANS_LINEAR) #Change these however you want
+	#tween.set_ease(Tween.EASE_IN_OUT) #Change these however you want
+	#
+	#var start_ratio: float = target_follow.progress_ratio
+	#var end_ratio: float = start_ratio + 1.0
+	##target_follow.progress = 10.0
+	#
+	#tween.tween_property(target_follow, "progress_ratio", end_ratio, pLen)
+	#tween.tween_callback(func():
+		#target_follow.progress_ratio = start_ratio
+	#)
+	#tween.set_loops() #Loops indefinitely
+
+func _physics_process(delta: float) -> void:
 	
-	var tween = create_tween()
+	if !isMoving or follow_node == null: # or physicsBody == null
+		return
 	
-	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	tween.set_trans(Tween.TRANS_LINEAR) #Change these however you want
-	tween.set_ease(Tween.EASE_IN_OUT) #Change these however you want
+	var path_length = curve.get_baked_length()
 	
-	var start_ratio: float = target_follow.progress_ratio
-	var end_ratio: float = start_ratio + 1.0
-	#target_follow.progress = 10.0
+	follow_node.progress += travel_speed * delta
 	
-	tween.tween_property(target_follow, "progress_ratio", end_ratio, pLen)
-	tween.tween_callback(func():
-		target_follow.progress_ratio = start_ratio
-	)
-	tween.set_loops() #Loops indefinitely
+	if follow_node.progress >= path_length:
+		follow_node.progress -= path_length
+	
+	"""
+	Code Devs Blood Writing Left On The Castle Walls:
+	ASHTON, THIS IS THE TANGENCY. TAKE IT. MAKE TITANFALL 2 LIKE WE WERE MEANT TO
+	"""
+	var dir = follow_node.global_position - self_node.global_position
+	#var dir = curve.sample_baked_with_rotation(follow_node.progress).basis.z #Asks the forward direction at the point
+	#self_node.rotation.y = atan2(dir.x, dir.z) + deg_to_rad(forward_direction)
+	self_node.global_position = follow_node.global_position #For animatablebody3d
+	#physicsBody.apply_central_force(apply_force()) #For RigidBody3d
+	#var self_2d = Vector2(self.global_position.x, self.global_position.z)
+	#var target_2d = Vector2(follow_node.global_position.x, follow_node.global_position.z)
+	#var theta = self_2d.angle_to(target_2d)
+	#myVelocity = Vector3(cos(theta)*100.0, 0.0, sin(theta) * 100.0)
+	myVelocity = dir / delta #The distance traveled, divided by delta for average per second
+
+func get_player_jumped_velocity() -> Vector3:
+	#Calcing this every frame instead of on jump may be inefficient,
+	#but it's easier overall.
+	#Oh also, player velocity seems to multiply overtime,
+	#So we multiply the velocity added here to balance it out.
+	return myVelocity * jumpVelocityRatio
