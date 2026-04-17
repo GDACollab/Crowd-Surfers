abe934115c2eac196a8eb2d90fd9a87968db7bcd
jazzberry-jam <79028708+jazzberry-jam@users.noreply.github.com>
2026-04-16 23:36:33 -0700

    typewriter voices scale to text speed

--- Scripts/Audio/voice_manager.gd
+++ Scripts/Audio/voice_manager.gd
@@ -1,20 +1,25 @@
 class_name VoiceManager
 extends Node
 
-@export var typewriter_voice_frequency = 2
+## Determines how often a character voice sound plays (every 1, 2, 3, etc. letters)
+@export var typewriter_voice_frequency : int = 2
 
-var character_voice = {
-	"Slip": "andro",
+## TODO: deprecate this dictionary and change FMOD voice parameter values to character names rather than voice archetypes
+var character_voice : Dictionary = {
+	"default": "default",
 	"Chef": "deep",
-	"Sam": "deep"
+	"Minny": "deep",
+	"Nyx": "andro",
+	"Pavo": "andro"
 }
 
-func _handle_typewriter_voice(dialogue_text: String, character: String, letter_index: int, alphanumerics_count: int) -> bool:
-	if (alphanumerics_count % typewriter_voice_frequency == 0):
+func _handle_typewriter_voice(dialogue_text: String, character: String, letter_index: int, alphanumerics_count: int, textSpeedScale: float) -> bool:
+	## `typewriter_voice_frequency` scaled respective to text speed
+	var typewriter_voice_frequency_scaled = floor(typewriter_voice_frequency * textSpeedScale) as int
+	
+	if (alphanumerics_count % typewriter_voice_frequency_scaled == 0):
 		var should_increment_alphanumeric_counter = true
 		var letter = dialogue_text[letter_index]
-		# FmodServer.load_bank("bank:/CHAR", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
-		# print("event exists") if FmodServer.check_event_path("event:/SFX/CHAR/typewriter_voice/typewriter_voice_hub") else print("event does not exist")
 		var letter_sound = FmodServer.create_event_instance("event:/SFX/CHAR/typewriter_voice/typewriter_voice_hub")
 		
 		letter_sound.set_parameter_by_name_with_label("voice", character_voice[character], false)
--- Scripts/Interface/dialogue_interface.gd
+++ Scripts/Interface/dialogue_interface.gd
@@ -28,8 +28,10 @@ extends Control
 @export var outlineSize : int = 5
 ## Determines if shader drawing varies in size
 @export var sketchyDraw := true
+@export var textSpeedScale : float = 1
 @onready var leftPortrait = $"Left Character Portrait/Portrait Image"
 @onready var rightPortrait = $"Right Character Portrait/Portrait Image"
+var textSpeedInMilliseconds : float = 0.05
 var dialoguePanels : Array[InterfacePanel]
 var dialogueLabel : Label
 var currentStory : InkStory
@@ -62,7 +64,7 @@ func _process(_delta: float) -> void:
 			_get_input()
 		else:
 			_skip_scroll()
-	#Choice selection
+	# Choice selection
 	if(choicesDisplayed):
 		if(Input.is_action_just_pressed("move_down")):
 			selectedButton._unhighlight()
@@ -83,35 +85,35 @@ func _process(_delta: float) -> void:
 
 func _get_input():
 	if(!awaitingAnimations):
-		#Continue story
+		# Continue story
 		if(currentStory.GetCanContinue()):
 			_continue_story()
-		#Display choices
+		# Display choices
 		elif(currentStory.GetCurrentChoices().size() > 0 and !choicesDisplayed):
 			_display_choices()
-		#Make choice
+		# Make choice
 		elif(currentStory.GetCurrentChoices().size() > 0 and choicesDisplayed):
 			currentStory.ChooseChoiceIndex(currentChoiceIndex)
 			_clear_choices()
 			_continue_story()
-		#End story
+		# End story
 		else:
 			Inky.EndDialogue()
 
-##Loads next line
+## Loads next line
 func _continue_story():
 	dialogueText = currentStory.Continue()
-	#Spawn new dialogue panel
+	# Spawn new dialogue panel
 	var newDialoguePanel = dialoguePanel.instantiate() as InterfacePanel
 	_handle_tags(currentStory.GetCurrentTags(), newDialoguePanel)
 
 ## Displays the current line 
 func _handle_panel_text(newDialoguePanel):
-	#Set anchor point
+	# Set anchor point
 	newDialoguePanel.set_anchors_preset(Control.PRESET_CENTER_BOTTOM)
 	dialoguePanels.push_front(newDialoguePanel)
 	
-	#Ensures panels do not overflow
+	# Ensures panels do not overflow
 	if(dialoguePanels.size() > 3):
 		_kill_panel(dialoguePanels[3])
 		
@@ -121,24 +123,25 @@ func _handle_panel_text(newDialoguePanel):
 	_animate_panels()
 	_do_typewriter_text()
 	
-# Displays text incrementally with a typewriter effect and calls typewriter voice sounds (animalese)
+## Displays text incrementally with a typewriter effect and calls typewriter voice sounds (animalese)
 func _do_typewriter_text():
 	isTyping = true
 	dialogueLabel.visible_characters = 0
 	dialogueLabel.text = dialogueText
-	var alphanumerics_count = 0 # count of every non-punctuation character because those are the only characters play animalese sounds for
+	var alphanumerics_count = 0 ## count of every non-punctuation character because those are the only characters we play animalese sounds for
 	
 	while dialogueLabel.visible_characters < dialogueText.length() - 1:
 		var letter_index = dialogueLabel.visible_characters; 
+		# TODO: currentSpeakerData.characterName always returns nil during testing. fix this
 		var character_name = currentSpeakerData.characterName if (currentSpeakerData != null) else "Slip"
-		var should_increment_alphanumeric_counter = true
 		
-		should_increment_alphanumeric_counter = $VoiceManager._handle_typewriter_voice(dialogueText, character_name, letter_index, alphanumerics_count)
+		var should_increment_alphanumeric_counter = $VoiceManager._handle_typewriter_voice(
+			dialogueText, character_name, letter_index, alphanumerics_count, textSpeedScale)
 		
 		dialogueLabel.visible_characters += 1
 		if (should_increment_alphanumeric_counter): alphanumerics_count += 1
 		
-		await get_tree().create_timer(0.05).timeout
+		await get_tree().create_timer(textSpeedInMilliseconds / textSpeedScale).timeout
 		
 	isTyping = false
 
@@ -245,14 +248,14 @@ func _handle_tags(currentTags, newPanel):
 			"speaker":
 				if(previousSpeakerTag != tagValue):
 					differentSpeaker = true
-				#Get speaker data
+				# Get speaker data
 				if(currentSpeakerData == null or currentSpeakerData.characterName != tagValue):
-					#Load correct speaker data
+					# Load correct speaker data
 					var resourcePath = "res://Assets/Dialogue/Character Dialogue Data/" + str(tagValue) + "DialogueData.tres"
 					if ResourceLoader.exists(resourcePath):
 						currentSpeakerData = load(resourcePath)
 						print("Loaded speaker data")
-				#Add character data attributes
+				# Add character data attributes
 				if(currentSpeakerData != null and currentSpeakerData.characterName == tagValue):
 					newPanel.modulate = currentSpeakerData.colour
 				else:
@@ -265,7 +268,7 @@ func _handle_tags(currentTags, newPanel):
 					slipSpoke = false
 					previousSpeaker = leftPortrait
 					currentSpeaker = rightPortrait
-					## Switches to new speaker
+					# Switches to new speaker
 					if(lastNonSlipSpeaker != tagValue):
 						var currentSpeakerPanel = currentSpeaker.get_parent()
 						print("New speaker detected!")
@@ -280,15 +283,15 @@ func _handle_tags(currentTags, newPanel):
 						var portraitTween2 = create_tween()
 						portraitTween2.tween_property(currentSpeakerPanel,"global_position", 
 						portraitPosition, portraitSwapTime/2).set_trans(transitionType).set_ease(easeType)
-					#Sets new panel to right side of screen
+					# Sets new panel to right side of screen
 					newPanel.pivot_offset = Vector2(dialoguePanels[0].size.x, 0)
-				#Adjust portraits based on speaker
+				# Adjust portraits based on speaker
 				currentSpeaker.scale = speakingPortraitScale
 				currentSpeaker.self_modulate = Color.WHITE
 				previousSpeaker.scale = Vector2.ONE
 				previousSpeaker.self_modulate = previousSpeakerColor
 				previousSpeakerTag = tagValue
-				## Portrait Outline
+				# Portrait Outline
 				if(differentSpeaker):
 					print("This speaker needs an outline")
 					previousSpeaker.material = null
