fba536c76ccb2c9adab560c7c534e95d18587fe0
jazzberry-jam <79028708+jazzberry-jam@users.noreply.github.com>
2026-04-16 19:24:52 -0700

    i'm the goat

--- Scripts/Singletons/global_fmod_bank_loader.gd
+++ Scripts/Singletons/global_fmod_bank_loader.gd
@@ -3,10 +3,11 @@ extends FmodBankLoader
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	FmodServer.load_bank("bank:/Master.strings", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	FmodServer.load_bank("bank:/Master", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	FmodServer.load_bank("bank:/Level", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	FmodServer.load_bank("bank:/UI", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	FmodServer.load_bank("bank:/CHAR", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
-	FmodServer.load_bank("bank:/Level", FmodServer.FMOD_STUDIO_LOAD_BANK_NORMAL)
 
 # # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta: float) -> void:
