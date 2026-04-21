64cdbce3d42bebe8e518930271b0143f5c7f7f49
FFreeze <quincymon@icloud.com>
2026-04-21 12:45:20 -0700

    I consider this good enough to work with for the most part

--- Scripts/camera_3d.gd
+++ Scripts/camera_3d.gd
@@ -41,10 +41,7 @@ func _process(delta: float) -> void:
 	
 	RenderingServer.global_shader_parameter_set("transparency_window_pos", transparency_window_pos)
 	RenderingServer.global_shader_parameter_set("transparency_window_scale", transparency_window_scale)
-	
 	RenderingServer.global_shader_parameter_set("player_pos", player.global_position)
-	RenderingServer.global_shader_parameter_set("far_plane", far)
-	RenderingServer.global_shader_parameter_set("near_plane", near)
 	
 	#
 	#ray.force_raycast_update()
