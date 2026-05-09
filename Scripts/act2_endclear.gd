d75c049c70d2986cc2ab3e770389d7cce7bfaa9c
DapperCoat <mrdappers12@gmail.com>
2026-05-08 18:19:11 -0700

    Added collision to clear blockade (it doesn't work)

---
+++ Scripts/act2_endclear.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var blockade : Node3D = $"../Blockade"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	blockade.visible = false
