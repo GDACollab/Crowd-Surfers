cb632e128c8af9f36043016d5c0fae4fec890c57
DapperCoat <mrdappers12@gmail.com>
2026-05-08 19:37:20 -0700

    Changes to Crowds and Buildings in Act 2

--- Scripts/act2_endclear.gd
+++ Scripts/act2_endclear.gd
@@ -14,9 +14,9 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	print("Entered checkpoint ", body.name)
 	if (body.name == "Player"):
-		blockade.x = 3100.0
-		blockade.y = 0
-		blockade.z = 100.0
+		blockade.position.x = 500.0
+		blockade.position.y = -159.337
+		blockade.position.z = 100.0
 		fence.position.x = 925.0
 		fence.position.y = 35.0
 		fence.position.x = 125.0
