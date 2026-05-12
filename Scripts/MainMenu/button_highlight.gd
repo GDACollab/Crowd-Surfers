88375d5af4e9b20172b7f4a54f8a15233d87882e
NinjaFromConsole <rothtyler24@gmail.com>
2026-05-11 22:35:40 -0700

    Better Buttons
    - Added click masks to all the main menu buttons for more accurate click and hover events
    - All main menu buttons now slightly brighten when hovering over them

---
+++ Scripts/MainMenu/button_highlight.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+@export var brightness_change: float = 0.2
+
+
+func _on_mouse_entered() -> void:
+	modulate.r += brightness_change
+	modulate.g += brightness_change
+	modulate.b += brightness_change
+
+func _on_mouse_exited() -> void:
+	modulate.r -= brightness_change
+	modulate.g -= brightness_change
+	modulate.b -= brightness_change
