81b28dbe39e5484fc9bcc1ed5601f959e4c0ce95
NinjaFromConsole <rothtyler24@gmail.com>
2026-05-17 16:00:33 -0700

    Split Music Notes
    - Each music note now alternates between moving up and down
    - Other minor title screen animation tweaks

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -23,16 +23,26 @@ func _ready():
 	now_playing_image_start_y = now_playing_image.position.y
 	play_button_start_y = play_button.position.y
 	credits_button_start_y = credits_button.position.y
+	
+	var index: int = 0
+	for note: TextureRect in music_notes:
+		if (index % 2 == 1):
+			note.position.y += music_notes_move_distance
+		index += 1
 
 func _process(delta: float):	
 	title_image.position.y = sin((timer * 2 * PI) / hover_time) * hover_magnitude + title_image_start_y
 	now_playing_image.position.y = sin(5 + (timer * 1.7 * PI) / hover_time) * hover_magnitude + now_playing_image_start_y
-	play_button.position.y = sin(10 + (timer * 1.8 * PI) / hover_time) * hover_magnitude + play_button_start_y
-	credits_button.position.y = sin(15 + (timer * 1.6 * PI) / hover_time) * hover_magnitude + credits_button_start_y
+	play_button.position.y = sin(10 + (timer * 1.8 * PI) / hover_time) * hover_magnitude * 0.8 + play_button_start_y
+	credits_button.position.y = sin(15 + (timer * 1.6 * PI) / hover_time) * hover_magnitude * 0.8 + credits_button_start_y
 	
 	if (floor((timer + delta) / music_notes_move_time) > floor((timer) / music_notes_move_time)):
+		# Index offsets the animation for every other note
+		var index: int = 0
 		for note: TextureRect in music_notes:
-			note.position.y += move_up * music_notes_move_distance
+			note.position.y += move_up * music_notes_move_distance * float(((index % 2) * 2) - 1)
+			index += 1
+			
 		move_up *= -1
 		
 	timer += delta
