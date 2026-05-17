4def7f51354045c36234855d39e363fdaf5e8ed5
NinjaFromConsole <rothtyler24@gmail.com>
2026-05-17 15:52:10 -0700

    Title Screen Animation
    - Buttons and title image hover up and down
    - Music notes do musicy things

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,5 +1,44 @@
 extends Control
 
+@onready var title_image: TextureRect = $Images/TitleImage
+@onready var now_playing_image: TextureRect = $Images/NowPlaying
+@onready var play_button: TextureButton = $PlayButton
+@onready var credits_button: TextureButton = $CreditsButton
+
+@export var music_notes: Array[TextureRect]
+@export var music_notes_move_time: float = 0.5
+@export var music_notes_move_distance: float = 15
+@export var hover_time: float = 4.0
+@export var hover_magnitude: float = 1.0
+
+var title_image_start_y: float
+var now_playing_image_start_y: float
+var play_button_start_y: float
+var credits_button_start_y: float
+var timer: float
+var move_up: int = -1
+
+func _ready():
+	title_image_start_y = title_image.position.y
+	now_playing_image_start_y = now_playing_image.position.y
+	play_button_start_y = play_button.position.y
+	credits_button_start_y = credits_button.position.y
+
+func _process(delta: float):	
+	title_image.position.y = sin((timer * 2 * PI) / hover_time) * hover_magnitude + title_image_start_y
+	now_playing_image.position.y = sin(5 + (timer * 1.7 * PI) / hover_time) * hover_magnitude + now_playing_image_start_y
+	play_button.position.y = sin(10 + (timer * 1.8 * PI) / hover_time) * hover_magnitude + play_button_start_y
+	credits_button.position.y = sin(15 + (timer * 1.6 * PI) / hover_time) * hover_magnitude + credits_button_start_y
+	
+	if (floor((timer + delta) / music_notes_move_time) > floor((timer) / music_notes_move_time)):
+		for note: TextureRect in music_notes:
+			note.position.y += move_up * music_notes_move_distance
+		move_up *= -1
+		
+	timer += delta
+
+	
+	
 func _on_credits_button_pressed() -> void:
 	SceneFadeTransition.transition_to_scene(load("res://Scenes/UI Menus/Credits/credits.tscn"))
 
