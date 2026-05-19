a272905b5236b94a32d0ec55a3e5994f3d403348
jazzberry-jam <79028708+jazzberry-jam@users.noreply.github.com>
2026-05-19 03:15:53 -0700

    voice lines and animalese added to fmod
    need to add sam animalese and tweak based on typewriter tests
    the dialogue interface scene currently breaks when i try to use it

--- Scripts/Interface/dialogue_interface.gd
+++ Scripts/Interface/dialogue_interface.gd
@@ -140,11 +140,11 @@ func _do_typewriter_text():
 		
 		var should_increment_alphanumeric_counter = $VoiceManager._handle_typewriter_voice(
 			dialogueText, character_name, letter_index, alphanumerics_count, textSpeedScale)
-			
-		if (should_increment_alphanumeric_counter): alphanumerics_count += 1
 		
 		dialogueLabel.visible_characters += 1
-		await get_tree().create_timer(0.02).timeout
+		if (should_increment_alphanumeric_counter): alphanumerics_count += 1
+		
+		await get_tree().create_timer(textSpeedInMilliseconds / textSpeedScale).timeout
 	isTyping = false
 
 ## Move old panels
