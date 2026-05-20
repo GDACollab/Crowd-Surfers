fd69ac333a5b56415e2c4422ebe0acf91e6e8cb0
JaydenRRamirez <ramirezrjayden@gmail.com>
2026-05-19 20:03:32 -0700

    Updated to do a Cutscene for the Opening.

--- Scenes/Cutscenes/cutscene_example.gd
+++ Scenes/Cutscenes/cutscene_example.gd
@@ -1,14 +1,5 @@
 extends Node
 
-func _ready() -> void:
-	$AnimationPlayer.play("Cutscene_Test")
-
-func pause_for_input():
-	$AnimationPlayer.pause()
-
-func _input(event):
-	# Listen for a left mouse click
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		# If the animation is currently paused, resume it
-		if not $AnimationPlayer.is_playing():
-			$AnimationPlayer.play()
+func _ready():
+	# Play the animation as soon as the scene loads
+	$AnimatedSprite2D.play("default")
