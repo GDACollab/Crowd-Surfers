4599aed5156e7c2923cf77d959960daabb6d08bd
JohnnyFisher27 <157189363+JohnnyFisher27@users.noreply.github.com>
2026-05-26 02:07:47 -0700

    fix merge conflict

--- Scripts/pause_holder.gd
+++ Scripts/pause_holder.gd
@@ -12,7 +12,7 @@ func _process(_delta: float) -> void:
 			var pause_screen: Node = pause_screen_scene.instantiate()
 			add_child(pause_screen)
 			get_tree().paused = true
-		else:
+		elif get_child(0).transitioning != true:
 			visible = false
 			get_tree().paused = false
 			get_child(0).queue_free()
