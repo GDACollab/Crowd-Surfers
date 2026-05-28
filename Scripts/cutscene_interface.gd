da32151cc01010d55bb7b8fa55d8e82aa5d38f4e
jazzberry-jam <79028708+jazzberry-jam@users.noreply.github.com>
2026-05-28 03:46:43 -0700

    FINISHED OPENING CUTSCENE

--- Scripts/cutscene_interface.gd
+++ Scripts/cutscene_interface.gd
@@ -10,31 +10,37 @@ extends Control
 ## Scene to transition to upon exit
 @export var scene_to_transition_to := "res://Scenes/UI Menus/TitleScreen.tscn"
 ## Cutscene fade-in transition time
-@export var fade_in_time := 10.
+@export var fade_in_time := 8.
 ## Cutscene fade-out transition time
-@export var fade_out_time := 5.
+@export var fade_out_time := 1.
 
+@onready var animator : AnimationPlayer = $InterfaceAnimator
 @onready var progress_bar : Node2D = $ProgressBar
-@onready var animator = $"InterfaceAnimator"
 @onready var wait_timer : float = abs(WAIT_TIME) 
 
 var animatic_frame_count : int
+var is_ending := false
 
 func _ready():
 	print("Cutscene: Cutscene has been started.")
 	
-	if (animatic_frames == null): animatic_frames = $DefaultCutsceneFrames
-	if (cutscene_sound == null): cutscene_sound = $DefaultCutsceneSound
+	if (animatic_frames == null): 
+		animatic_frames = $DefaultCutsceneFrames
+		animatic_frames.visible = true
+		
+	if (cutscene_sound == null): 
+		cutscene_sound = $DefaultCutsceneSound
 	
 	animatic_frame_count = animatic_frames.sprite_frames.get_frame_count("default")
 	progress_bar.scale.x = 0
 	
 	cutscene_sound.play()
-	animator.play("fade_in", 1/abs(fade_in_time))
+	animator.play("fade_in", -1, 1/abs(fade_in_time))
 
 func _process(delta):
 	if (Input.is_action_just_pressed("DialogueInteract") and wait_timer <= 0.):
 		if (animatic_frames.frame + 1 >= animatic_frame_count):
+			is_ending = true
 			_end_cutscene()
 		
 		progress_bar.scale.x = 0
@@ -46,12 +52,13 @@ func _process(delta):
 	if (Input.is_action_just_pressed("DialogueCancel")):
 		_end_cutscene()
 		
-	wait_timer -= delta
-	progress_bar.scale.x += delta/(WAIT_TIME + 0.1) # Divide by zero handling
+	if (!is_ending): 
+		wait_timer -= delta
+		progress_bar.scale.x += delta/(WAIT_TIME + 0.1) # Divide by zero handling
 
 func _end_cutscene():
 	print("Cutscene: Cutscene has been stopped.")
 	cutscene_sound.stop()
-	animator.play("fade_out", 1/abs(fade_out_time))
-	await animator.animation_finished
+	animator.play("fade_out", -1, 1/abs(fade_out_time))
+	await get_tree().create_timer(1/abs(fade_out_time)).timeout
 	SceneFadeTransition.transition_to_scene(load(scene_to_transition_to))
