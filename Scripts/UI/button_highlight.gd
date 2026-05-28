24f8f699daa4b868f5a4c14b2119e80c4c60a0c9
tatemaguire <tatemaguir@gmail.com>
2026-05-28 02:55:47 -0700

    controller UI input map. button highlight support for focus. titlescreen implemented

--- Scripts/UI/button_highlight.gd
+++ Scripts/UI/button_highlight.gd
@@ -1,6 +1,7 @@
 extends TextureButton
 
 @export var brightness_change: float = 0.2
+@export var focus_brightness_change: float = 0.8
 
 
 func _on_mouse_entered() -> void:
@@ -18,3 +19,20 @@ func _on_mouse_exited() -> void:
 	modulate.r -= brightness_change
 	modulate.g -= brightness_change
 	modulate.b -= brightness_change
+
+
+func _on_focus_entered() -> void:
+	if (self.disabled == true):
+		return
+		
+	modulate.r += focus_brightness_change
+	modulate.g += focus_brightness_change
+	modulate.b += focus_brightness_change
+
+func _on_focus_exited() -> void:
+	if (self.disabled == true):
+		return
+		
+	modulate.r -= focus_brightness_change
+	modulate.g -= focus_brightness_change
+	modulate.b -= focus_brightness_change
