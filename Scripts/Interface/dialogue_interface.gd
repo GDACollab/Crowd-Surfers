519a4b37890fa4f1ae8e06e165d39a235cf0798b
jazzberry-jam <79028708+jazzberry-jam@users.noreply.github.com>
2026-05-28 16:56:44 -0700

    UI changes to dialogue interface scene for parity with cutscene interface

--- Scripts/Interface/dialogue_interface.gd
+++ Scripts/Interface/dialogue_interface.gd
@@ -89,6 +89,9 @@ func _ready() -> void:
 	_load_main_panels()
 	_get_input()
 	
+	$ContinueButton.pressed.connect(_on_continue_button_pressed)
+	$SkipButton.pressed.connect(_on_skip_button_pressed)
+	
 	music_bus = FmodServer.get_bus("bus:/MUS")
 	Audio.set_bus_volume("bus:/MUS", music_bus.volume * music_bus_volume_scalar)
 	
@@ -99,13 +102,14 @@ func _ready() -> void:
 		
 	else:
 		Audio.unpause_persistent("mus_hub")
-		
+
 func _exit_tree() -> void:
 	Audio.set_bus_volume("bus:/MUS", music_bus.volume / music_bus_volume_scalar)
 	Audio.kill_persistent("mus_hub")
 
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("DialogueCancel") and can_skip):
+		FmodServer.play_one_shot("event:/SFX/UI/menu_back")
 		Inky.EndDialogue()
 		
 	if(Input.is_action_just_pressed("DialogueInteract")):
