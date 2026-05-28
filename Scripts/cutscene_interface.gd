582fbc1b4e801c819c0db55b76feeea42882d4fa
jazzberry-jam <79028708+jazzberry-jam@users.noreply.github.com>
2026-05-27 20:01:19 -0700

    Cutscene sound progress update

--- Scripts/cutscene_interface.gd
+++ Scripts/cutscene_interface.gd
@@ -3,11 +3,16 @@ extends Control
 ## Wait time so the player doesn't spam through the cutscene, measured in seconds
 @export var WAIT_TIME := 2.
 ## Animation frames for animatic. If no AnimatedSprite2D is assigned, defaults to opening cutscene.
+## NOTE: if the animation name is not "default" this script breaks D:
 @export var animatic_frames : AnimatedSprite2D
 ## FMOD event for animatic. If no FmodEventEmitter2D is assigned, defaults to opening cutscene event.
 @export var cutscene_sound : FmodEventEmitter2D
 ## Scene to transition to upon exit
 @export var scene_to_transition_to := "res://Scenes/UI Menus/TitleScreen.tscn"
+## Cutscene fade-in transition time
+@export var fade_in_time := 10.
+## Cutscene fade-out transition time
+@export var fade_out_time := 5.
 
 @onready var progress_bar : Node2D = $ProgressBar
 @onready var animator = $"InterfaceAnimator"
@@ -16,16 +21,16 @@ extends Control
 var animatic_frame_count : int
 
 func _ready():
-	print("Cutscene: Cutscene has been started")
+	print("Cutscene: Cutscene has been started.")
 	
 	if (animatic_frames == null): animatic_frames = $DefaultCutsceneFrames
 	if (cutscene_sound == null): cutscene_sound = $DefaultCutsceneSound
 	
 	animatic_frame_count = animatic_frames.sprite_frames.get_frame_count("default")
+	progress_bar.scale.x = 0
 	
 	cutscene_sound.play()
-	animator.play("fade_in")
-	progress_bar.scale.x = 0
+	animator.play("fade_in", 1/abs(fade_in_time))
 
 func _process(delta):
 	if (Input.is_action_just_pressed("DialogueInteract") and wait_timer <= 0.):
@@ -45,8 +50,8 @@ func _process(delta):
 	progress_bar.scale.x += delta/(WAIT_TIME + 0.1) # Divide by zero handling
 
 func _end_cutscene():
-	print("Cutscene: Cutscene has been stopped")
+	print("Cutscene: Cutscene has been stopped.")
 	cutscene_sound.stop()
-	animator.play("fade_out")
+	animator.play("fade_out", 1/abs(fade_out_time))
 	await animator.animation_finished
 	SceneFadeTransition.transition_to_scene(load(scene_to_transition_to))
